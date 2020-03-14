5aae4263f6d05009298b1b8bd63aa34c2e31ef29
antonmacko <48652152+antonmacko@users.noreply.github.com>
2020-03-14 18:32:14 +0100

    Create deobfuscation_xor.py

---
+++ deobfuscation_xor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+args=sys.stdin
+for line in args:
+   out=""
+   for i in line[::-1]:
+        print i
+        out+=chr(ord(i)^((len(line))-line.find(i)))
+
+   print line[:len(line)-1]+":"+out[1:]
