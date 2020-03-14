7a900116228673ee94af502fa3744f5c9c033476
antonmacko <48652152+antonmacko@users.noreply.github.com>
2020-03-14 18:48:19 +0100

    Update deobfuscation_xor.py

--- deobfuscation_xor.py
+++ deobfuscation_xor.py
@@ -5,6 +5,6 @@ for line in args:
    out=""
    for i in line[::-1]:
         print i
-        out+=chr(ord(i)^((len(line))-line.find(i)))
+        out+=chr(ord(i)^((len(line))-line.find(i)-1))
 
    print line[:len(line)-1]+":"+out[1:]
